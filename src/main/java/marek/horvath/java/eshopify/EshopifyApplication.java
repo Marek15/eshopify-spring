962713a7264ce8718b2195e0b2f8d48ac02d765b
marekhorvath-ui <marek.horvath@onesimplicity.com>
2026-08-19 14:29:50 +0200

    initial commit

---
+++ src/main/java/marek/horvath/java/eshopify/EshopifyApplication.java
@@ -0,0 +1,13 @@
+package marek.horvath.java.eshopify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/marek/horvath/java/eshopify/EshopifyApplicationTests.java
@@ -0,0 +1,13 @@
+package marek.horvath.java.eshopify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
