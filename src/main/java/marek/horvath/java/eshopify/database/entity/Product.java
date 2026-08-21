b9735a893fcf53d1ce2a032cfb9a88ba83e0789f
marekhorvath-ui <marek.horvath@onesimplicity.com>
2026-08-21 22:47:49 +0200

    feature: setup database, migration and first entity

---
+++ src/main/java/marek/horvath/java/eshopify/database/entity/Product.java
@@ -0,0 +1,26 @@
+package marek.horvath.java.eshopify.database.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String price;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
